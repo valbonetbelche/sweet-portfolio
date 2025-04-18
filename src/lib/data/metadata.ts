6d70ac7a442cc0b9e637ac19f8050917feaa644d
Valentin Bonetbelche <valbonetbelche@gmail.com>
2025-04-18 15:18:02 +0200

    Added custom metadata and favicon

---
+++ src/lib/data/metadata.ts
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Valentin Bonetbelche – Full Stack Developer & DevOps Engineer",
+  icons: {
+    icon: "/favicon/favicon.ico",
+  },
+  description:
+    "Welcome to the personal portfolio of Valentin Bonetbelche – Full Stack Developer, DevOps/SRE Engineer, and Indie Hacker. Explore personal projects, freelance work, and professional experience.",
+  authors: [{ name: "Valentin Bonetbelche", url: "https://valbonetbelche.com" }],
+  keywords: [
+    "Valentin Bonetbelche",
+    "Full Stack Developer",
+    "DevOps Engineer",
+    "SRE",
+    "Freelance Developer",
+    "Next.js Portfolio",
+    "ShadCN UI",
+    "Tailwind Developer",
+    "TypeScript Developer",
+    "Kalend App",
+    "Faststack.dev",
+    "Appaway",
+  ],
+  openGraph: {
+    title: "Valentin Bonetbelche – Full Stack Dev & Indie Hacker",
+    description:
+      "Discover the work and journey of Valentin Bonetbelche – full stack engineer and creator of tools like Faststack.dev, Kalend, and Appaway.",
+    url: "https://valbonetbelche.com",
+    siteName: "Valentin Bonetbelche",
+    locale: "en_US",
+    type: "website",
+  },
+  metadataBase: new URL("https://valbonetbelche.com"),
+}
